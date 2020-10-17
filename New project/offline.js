3a6ce2f66906ce70cf610d2688629fc578bd32c1
Kyraaikawa <73035248+Kyraaikawa@users.noreply.github.com>
2020-10-18 00:52:46 +0700

    Add files via upload

---
+++ New project/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1602954945,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/particles.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/3ab84d9017cff0ed565bb42fa90712cc-sheet0.png",
+		"images/85fcf5e9e70c86e7fb7439ea09a2b6f-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
